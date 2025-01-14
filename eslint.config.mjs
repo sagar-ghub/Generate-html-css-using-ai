1911242c3280d29dd789f10d3145f16b7ed22ba3
Sagar <sagarmohanty5509@gmail.com>
2025-01-14 19:03:36 +0530

    eslient

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,8 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
 
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
   }),
 ];
